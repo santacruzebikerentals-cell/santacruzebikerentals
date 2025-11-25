327e16d186fbcafa60d9ff323ed000faaa4518b9
santacruzebikerentals-cell <santacruzebikerentals@gmail.com>
2025-11-25 08:41:00 -0800

    external network allow

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,         // allow access from network / external URLs
+    port: 5173,         // your dev server port
+    allowedHosts: 'all' // allow ngrok and any other hosts
+  },
 })
